91186f53ba6843d8c77e07c00eb11b66d8d48f9c
anilamurali <anilamurali98@gmail.com>
2022-05-24 10:29:32 +0530

    first commit

--- src/Component/Searchbus.js
+++ src/Component/Searchbus.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Navbar from './Navbar'
 
 const Searchbus = () => {
     var [route,setRoute]=useState("")
@@ -8,6 +9,7 @@ const Searchbus = () => {
     }
   return (
     <div>
+        <Navbar/>
         <div class="container">
     <div class="row">
         <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
