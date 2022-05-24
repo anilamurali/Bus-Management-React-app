9f99aec82130b982b76b085115bae01d69caec19
anilamurali <anilamurali98@gmail.com>
2022-05-24 08:39:57 +0530

    First commit

---
+++ src/Component/Addbus.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+const Addbus = () => {
+    var [route,setRoute]=useState("")
+    var [name,setName]=useState("")
+    var [regno,setReg]=useState("")
+    var [owner,setOwner]=useState("")
+    var [contact,setContact]=useState("")
+    const setData=()=>{
+        var data={"route":route,"name":name,"regno":regno,"owner":owner,"contact":contact}
+        console.log(data)
+    }
+  return (
+    <div><div class="container">
+    <div class="row">
+        <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div class="row g-3">
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" class="form-label">Route</label>
+                    <input onChange={(e)=>{setRoute(e.target.value)}} type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" class="form-label">Bus Name</label>
+                    <input onChange={(e)=>{setName(e.target.value)}} type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" class="form-label">Bus Register Number</label>
+                    <input onChange={(e)=>{setReg(e.target.value)}} type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" class="form-label">Owner Name</label>
+                    <input onChange={(e)=>{setOwner(e.target.value)}} type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" class="form-label">Contact Number</label>
+                    <input onChange={(e)=>{setContact(e.target.value)}} type="text" class="form-control"/>
+                </div>
+                
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button onClick={setData} class="btn btn-dark">SUBMIT</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div></div>
+  )
+}
+
+export default Addbus
